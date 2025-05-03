eb32d47a06483c9e38d62b441aceae84acdf9883
joelll7 <jsmunoz@pnw.edu>
2025-05-02 22:04:54 -0500

    Added Navigation bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,10 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+</script>
+
 <template>
   <div class="min-h-screen bg-gray-50">
+    <Navbar />
     <router-view />
   </div>
 </template>
-
-<script setup></script>
